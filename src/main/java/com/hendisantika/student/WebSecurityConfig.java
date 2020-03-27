6985d8383323431d942df372eb5fc71a73d62e82
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-27 18:56:41 +0700

    #23 - Bug Fix Security password

--- src/main/java/com/hendisantika/student/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/student/WebSecurityConfig.java
@@ -4,38 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        
-    	http
-    		.authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
-    			.and()
-            .authorizeRequests()
-                .antMatchers("/", "add", "delete/{id}", "save", "students").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .defaultSuccessUrl("/students")
-                .permitAll()
-            	.and()
-            .logout()
-            	.permitAll()
-            	.and()
-            .httpBasic()
-            	.and()
-            .csrf().disable(); //Disable CSRF
-    }
-     
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {  
-    	auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
-//				.and().withUser("admin").password("admin");
-    }
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
+				.and()
+				.authorizeRequests()
+				.antMatchers("/", "add", "delete/{id}", "save", "students").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.defaultSuccessUrl("/students")
+				.permitAll()
+				.and()
+				.logout()
+				.permitAll()
+				.and()
+				.httpBasic()
+				.and()
+				.csrf().disable(); //Disable CSRF
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("user").password(("{noop}user")).roles("USER")
+				.and()
+				.withUser("admin").password(("{noop}admin")).roles("ADMIN");
+	}
+
 }
